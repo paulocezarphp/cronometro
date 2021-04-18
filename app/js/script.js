eb2d447a75f5d5a19c1c9ad1332036a3f9c1aade
Cezar Leigos <cezarleigos28@outlook.com>
2021-04-18 20:07:15 -0300

    v0.2 - Implementando funções e definindo painel
    Dessa vez nós criamos as funções que serão ativadas pelo usuário e também começamos a definir a aparência que o painel do aplicativo terá.

---
+++ app/js/script.js
@@ -0,0 +1,36 @@
+function Play(){
+
+}
+
+function Stop(){
+	
+}
+
+function Pause(){
+	
+}
+
+function AlmentarHora(){
+	
+}
+
+function DiminuirHora(){
+	
+}
+
+function AlmentarMinuto(){
+	
+}
+
+function DiminuirMinuto(){
+	
+}
+
+function AlmentarSegundo(){
+	
+}
+
+function DiminuirSegundo(){
+	
+}
+
