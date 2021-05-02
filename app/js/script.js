43f68aef4f8ec24f04f1b00c4aed3377e4dd8f8a
Cezar Leigos <cezarleigos28@outlook.com>
2021-05-02 19:28:13 -0300

    v0.5 - Finalizando painel
    Dessa vez nós começamos a implementar as funções que farão o nosso aplicativo rodar, começamos a criar a função de "Play","Pause" e "Stop". Também finalizamos a estilização dos elementos na folha de estilo.

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,18 @@
-var hora = 0;
+var horas = 0;
 var minuto = 0;
 var segundo = 0;
+var estado = "stop"; //play, pause, stop
 
 function Play(){
-
+	estado = "play";
 }
 
 function Stop(){
-	
+	estado = "stop";
 }
 
 function Pause(){
-	
+	estado = "pause";
 }
 
 function AlmentarHora(){
@@ -23,7 +24,7 @@ function DiminuirHora(){
 }
 
 function AlmentarMinuto(){
-	
+
 }
 
 function DiminuirMinuto(){
@@ -38,3 +39,26 @@ function DiminuirSegundo(){
 	
 }
 
+function ExecutaTempo(){
+    if(estado == "stop"){
+
+    }else if(estado == "pause"){
+
+    }else if(estado == "play"){
+    	
+    }else{
+
+    }
+}
+
+document.getElementById("botao-play").addEventListener("click", Play);
+document.getElementById("botao-pause").addEventListener("click", Pause);
+document.getElementById("botao-stop").addEventListener("click", Stop);
+document.getElementById("h_cima").addEventListener("click", AlmentarHora);
+document.getElementById("h_baixo").addEventListener("click", DiminuirHora);
+document.getElementById("m_cima").addEventListener("click", AlmentarMinuto);
+document.getElementById("m_baixo").addEventListener("click", DiminuirMinuto);
+document.getElementById("s_cima").addEventListener("click", AlmentarSegundo);
+document.getElementById("s_baixo").addEventListener("click", DiminuirSegundo);
+
+setInterval(ExecutaTempo,1000);
